46cfe6fceab5644064951237db49a192f267dceb
suresh babu <94280551+sureshBabu1999@users.noreply.github.com>
2021-11-23 22:11:05 +0530

    Add files via upload

---
+++ 3_implementation/inc/header.h
@@ -0,0 +1,25 @@
+/**
+ * @file header.h
+ * @author your name (you@domain.com)
+ * @brief Library management system with 6 menu options
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * @brief 
+ * 
+ * @param operand1 
+ * @param operand2 
+ * @return int 
+ */
+int add(int operand1, int operand2);
+
+
