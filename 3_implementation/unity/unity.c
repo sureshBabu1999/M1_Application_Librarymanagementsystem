97cb39e2fac7b64607d183da07e4a076a49049b5
suresh babu <94280551+sureshBabu1999@users.noreply.github.com>
2021-11-24 07:51:27 +0530

    Update unity.c

--- 3_implementation/unity/unity.c
+++ 3_implementation/unity/unity.c
@@ -1,1 +1,21 @@
 
+/**
+ * @file Project_approach.h
+ * @author suresh (you@domain.com)
+ * @brief string library and standard library functions
+ */
+#include<stdlib.h>
+#include<string.h>
+
+
+/**
+ * @brief structure for storing data
+ * 
+ */
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
