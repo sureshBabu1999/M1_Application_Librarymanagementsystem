69e499b32835603aa019cd3d72427ce6696fbd82
suresh babu <94280551+sureshBabu1999@users.noreply.github.com>
2021-11-24 07:50:00 +0530

    Update project_operations.c

--- 3_implementation/src/project_operations.c
+++ 3_implementation/src/project_operations.c
@@ -1,1 +1,30 @@
 
+/**
+ * @brief structure for storing data
+ * 
+ */
+struct library
+{
+char bk_name[30];
+char author[30];
+int pages;
+float price;
+};
+
+/**
+ * @brief keeps add the inputs
+ * 
+ */
+for (i=0; i<keepcount; i++)
+
+/**
+ * @brief if the input matchs to the logic it takes input as the author name
+ * 
+ */
+if (strcmp(ar_nm, l[i].author) == 0)
+
+/**
+ * @brief if the input matchs to the logic it takes input as book name
+ * 
+ */
+if (strcmp(bk_nm, l[i].bk_name) == 0)
